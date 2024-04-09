742ccc207f91cb9302f895be8576ceec121d1a45
Lucas Madrigal <lucasmadrigalb@gmail.com>
2024-04-09 02:01:35 -0300

    detalles de pelis done - se limpian logs

--- detail.js
+++ detail.js
@@ -1,16 +1,12 @@
 import movies from "./data.js";
 
-let url = new URLSearchParams(location.search)
-let id = url.get("id")
-console.log(id);
+let url = new URLSearchParams(location.search);
+let id = url.get("id");
 
-console.log(movies);
-
-const Personaje = (arr, key) => arr.find(obj => obj.id === key)
-console.log("🚀 ~ personaje:", Personaje(movies,id))
+const Personaje = (arr, key) => arr.find((obj) => obj.id === key);
 
 function generarPaginaDetalles(pelicula) {
-    return `
+  return `
         <div>
             <h1>${pelicula.title}</h1>
             <p>${pelicula.overview}</p>
@@ -18,10 +14,9 @@ function generarPaginaDetalles(pelicula) {
     `;
 }
 
-const movieDetails = generarPaginaDetalles(Personaje(movies,id))
-const bannerContainer = document.getElementById("banner_container")
-const urlImageBanner = Personaje(movies,id).image
-console.log("🚀 ~ urlImageBanner:", urlImageBanner)
-bannerContainer.style.backgroundImage = `url('${urlImageBanner}')`
-const contenido = document.getElementById('movie_details--container');
+const movieDetails = generarPaginaDetalles(Personaje(movies, id));
+const bannerContainer = document.getElementById("banner_container");
+const urlImageBanner = Personaje(movies, id).image;
+bannerContainer.style.backgroundImage = `url('${urlImageBanner}')`;
+const contenido = document.getElementById("movie_details--container");
 contenido.innerHTML = movieDetails;
