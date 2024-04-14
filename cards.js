93f7600d8b7bfa1a3330087b33f8f185f3571161
Lucas Madrigal <lucasmadrigalb@gmail.com>
2024-04-13 22:28:02 -0300

    wip

--- cards.js
+++ cards.js
@@ -1,16 +1,63 @@
-import movies from "./data.js";
 const contenedor = document.getElementById("card--container");
 const select = document.getElementById("genresSelect");
 const searchInput = document.getElementById("search_input");
-const detailsButton = document.getElementById("details_button")
+const detailsButton = document.getElementById("details_button");
+
+const apiKey = "0ff70d54-dc0b-4262-9c3d-776cb0f34dbd";
+const URL = "https://moviestack.onrender.com/api/movies";
 
 var filteredMovies = [];
 var template = "";
+let movies = [];
+const renderizarCards = (pelis) => {
+  contenedor.innerHTML = "";
+  template = "";
+  pelis.map((movie) => {
+    template += generarCard(movie);
+  });
+
+  contenedor.innerHTML = template;
+};
+fetch(URL, {
+  headers: {
+    "x-api-key": apiKey,
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    movies = data.movies;
+    console.log("🚀 ~ data:", data);
+    console.log("🚀 ~ movies:", movies);
+    renderizarCards(movies);
+  })
+  .catch((error) => {
+    console.error("Error al realizar la solicitud fetch:", error);
+  });
+
+function toggleFavorito(id) {
+  let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+
+  const index = favoritos.indexOf(id);
+  if (index === -1) {
+    favoritos.push(id);
+    console.log("Elemento agregado a favoritos:", id);
+  } else {
+    favoritos.splice(index, 1);
+    console.log("Elemento eliminado de favoritos:", id);
+  }
+
+  localStorage.setItem("favoritos", JSON.stringify(favoritos));
+}
+
+document.getElementById("elemento").addEventListener("click", function () {
+  let id = this.getAttribute("data-id");
+  toggleFavorito(id);
+});
 
 function generarCard(movie) {
   return `
     <div class="h-64 w-1/5 p-2 border-solid	border-black border-2 ">
-        <img src="${movie.image}" class="card-img-top" alt="main_image_movie">
+        <img src="https://moviestack.onrender.com/static/${movie.image}" class="card-img-top" alt="main_image_movie">
         <div class="card-body">
             <h5 class=" text-base">${movie.title}</h5>
             <p class="card-text">${movie.tagline}</p>
@@ -22,18 +69,6 @@ function generarCard(movie) {
     `;
 }
 
-const renderizarCards = (pelis) => {
-  contenedor.innerHTML = "";
-  template = "";
-  pelis.map((movie) => {
-    template += generarCard(movie);
-  });
-
-  contenedor.innerHTML = template;
-};
-
-renderizarCards(movies);
-
 function filtrarPeliculasPorGenero() {
   const selectedGenre = select.value;
 
@@ -44,22 +79,20 @@ function filtrarPeliculasPorGenero() {
   renderizarCards(filteredMovies);
 }
 
-
 select.addEventListener("change", filtrarPeliculasPorGenero);
 
 searchInput.addEventListener("input", () => {
-  let search = searchInput.value
+  let search = searchInput.value;
 
   if (filteredMovies.length > 0) {
-    const filter = filteredMovies.filter(movie => movie.title.toLowerCase().includes(search.toLowerCase()))
-    renderizarCards(filter)
-  }
-  else{
-    const filter = movies.filter(movie => movie.title.toLowerCase().includes(search.toLowerCase()))
-    renderizarCards(filter)
+    const filter = filteredMovies.filter((movie) =>
+      movie.title.toLowerCase().includes(search.toLowerCase())
+    );
+    renderizarCards(filter);
+  } else {
+    const filter = movies.filter((movie) =>
+      movie.title.toLowerCase().includes(search.toLowerCase())
+    );
+    renderizarCards(filter);
   }
 });
-
-detailsButton.addEventListener("click", () => {
-
-})
